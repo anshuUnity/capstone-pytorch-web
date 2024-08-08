810e293b364ff06ec79c7ec8a8e70580966ef15a
Anshu Pal <64951054+anshuUnity@users.noreply.github.com>
2024-08-08 21:12:35 +0530

    Update settings.py

--- calory_counter/settings.py
+++ calory_counter/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 MODEL_PATH = BASE_DIR / "model_final.pth"
-OPEN_AI_KEY = ""
+OPEN_AI_KEY = "sk-proj-odoWbiwH94p5YCz3sED7T3BlbkFJHHMESfoQqfe7XjWmiLOT"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
