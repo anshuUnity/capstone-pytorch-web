ccd25d4cc3f6e143cf6e0e8c367d3e483548f047
Anshu Pal <64951054+anshuUnity@users.noreply.github.com>
2024-08-08 21:19:58 +0530

    vercel setup

--- calory_counter/settings.py
+++ calory_counter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-mgnt7ufk5$ax02(_%9wo-u@+00i#&3%i5k!ql)1)2k)b-r(fdz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
